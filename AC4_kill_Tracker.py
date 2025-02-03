671c0222b322bb3026a4e21763349dade32fd37b
TpRedNinja <152395020+TpRedNinja@users.noreply.github.com>
2025-02-03 15:31:24 -0500

    AC4_kill_Tracker.py
    Update it so it can read the pointer from a json file so its easier to replace the pointer if it ever goes bad
    Also made it to write the value to text file called totalkills.txt

--- AC4_kill_Tracker.py
+++ AC4_kill_Tracker.py
@@ -1,6 +1,7 @@
 import pymem
 import tkinter as tk
 from tkinter import ttk
+import json
 
 def test_process_connection(process_name):
     try:
@@ -16,17 +17,30 @@ def connect_to_game(process_name):
     else:
         return None
 
-def read_pointer(pm, base_address, offset1, offset2):
-    """
-    Reads memory value using a base address and two offsets.
-    """
+def read_pointer(pm, base_address, offsets):
     try:
         address = pm.read_uint(base_address)
-        address = pm.read_uint(address + offset1)
-        final_address = address + offset2
-        return pm.read_int(final_address)  # Read the final value
+        for offset in offsets[:-1]:
+            address = pm.read_uint(address + offset)
+        final_address = address + offsets[-1]
+        return pm.read_int(final_address)
     except pymem.exception.MemoryReadError as e:
         return None
+
+def read_pointer_from_file():
+    try:
+        with open('pointer.json', 'r') as f:
+            data = json.load(f)
+        base_address = int(data['base_address'], 16)
+        offsets = [int(offset, 16) for offset in data['offsets']]
+        return base_address, offsets
+    except FileNotFoundError:
+        print("pointer.json not found. Please create it with the correct pointer information.")
+        return None, None
+
+def write_kills_to_file(kills_value):
+    with open('totalkills.txt', 'w') as f:
+        f.write(str(kills_value))
 
 class GameGUI:
     def __init__(self, master):
@@ -53,9 +67,22 @@ class GameGUI:
         self.kills_label = tk.Label(master, text="Total Kills: N/A", bg='black', fg='white')
         self.kills_label.pack(pady=5)
 
+        self.pointer_label = tk.Label(master, text="Current Pointer: N/A", bg='black', fg='white')
+        self.pointer_label.pack(pady=5)
+
         self.pm = None
         self.current_process = None
         self.update_loop = None
+
+        self.base_address, self.offsets = read_pointer_from_file()
+        self.update_pointer_label()
+
+    def update_pointer_label(self):
+        if self.base_address is not None and self.offsets is not None:
+            offset_str = ", ".join([f"+{hex(offset)}" for offset in self.offsets])
+            self.pointer_label.config(text=f"Current Pointer: Base+{hex(self.base_address)}, {offset_str}")
+        else:
+            self.pointer_label.config(text="Current Pointer: Invalid pointer information")
 
     def connect_to_game(self):
         selected_game = self.selected_game.get()
@@ -73,6 +100,7 @@ class GameGUI:
             self.connect_button.config(state=tk.DISABLED)
             self.disconnect_button.config(state=tk.NORMAL)
             self.start_update_loop()
+            self.update_pointer_label()
         else:
             self.connection_label.config(text="Error: Cannot connect to process. Make sure the game is running.")
 
@@ -89,20 +117,18 @@ class GameGUI:
 
     def start_update_loop(self):
         self.update_kills()
-        self.update_loop = self.master.after(1000, self.start_update_loop)  # Update every 1000ms (1 second)
+        self.update_loop = self.master.after(1000, self.start_update_loop)
 
     def update_kills(self):
         if self.pm and self.current_process == "AC4BFSP.exe":
             try:
                 module = pymem.process.module_from_name(self.pm.process_handle, self.current_process)
-                base_address = module.lpBaseOfDll + 0x01798904  # Base address + static offset
-                offset1 = 0x338
-                offset2 = 0x4
-
-                kills_value = read_pointer(self.pm, base_address, offset1, offset2)
+                base_address = module.lpBaseOfDll + self.base_address
+                kills_value = read_pointer(self.pm, base_address, self.offsets)
                 
                 if kills_value is not None:
                     self.kills_label.config(text=f"Total Kills: {kills_value}")
+                    write_kills_to_file(kills_value)
                 else:
                     self.kills_label.config(text="Failed to read total kills")
             except Exception as e:
@@ -116,4 +142,3 @@ root = tk.Tk()
 root.geometry("800x500")
 gui = GameGUI(root)
 root.mainloop()
-cd C:\Users\jjdom\OneDrive\DesktopC:\Users\jjdom\OneDrive\Desktop
